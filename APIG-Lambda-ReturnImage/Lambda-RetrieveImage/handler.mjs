c955a9fcb4cf174e621497a0905595e8485202e2
Joe Losinski <128616881+jlosaws@users.noreply.github.com>
2024-10-01 22:17:22 -0400

    Initial Commit
    Terraform script to deploy a regional REST API Gateway with a single resource path and Lambda proxy integration, where Lambda will return an image.

---
+++ APIG-Lambda-ReturnImage/Lambda-RetrieveImage/handler.mjs
@@ -0,0 +1,28 @@
+import * as fs from 'node:fs/promises'; 
+
+export const lambda_handler = async (event) => {
+
+  var base64body;
+
+  try {
+    const img = await fs.readFile('./images/de.png');
+    base64body = Buffer.from(img).toString('base64');
+  } catch (err) {
+    console.error(err);
+  }
+  
+  const response = {
+    'headers': { 
+        "Content-Type": "image/png", 
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers": "Content-Type,X-Amz-Date,Authorization,X-Api-Key,X-Amz-Security-Token"
+      },
+    'statusCode': 200,
+    'body': base64body,
+    'isBase64Encoded': true
+  }
+
+  console.log(response);
+
+  return response
+}
